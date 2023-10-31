bebff64e8df103ae62b0340cdb89d3f2ff748725
Erik Menkin Lysfjord <erik.m.lysfjord@ntnu.no>
2023-10-31 12:47:43 +0100

    test: create tests for actionbar
    Closes #49
    
    See merge request it2810-h23/Team-15/prosjekt-2!33
    
    Co-authored-by: Erik Menkin Lysfjord <erikmly@stud.ntnu.no>

---
+++ frontend/src/components/actionbar/Actionbar.test.tsx
@@ -0,0 +1,41 @@
+import { describe, it, expect } from "vitest";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Actionbar from "./Actionbar";
+import { act } from "react-dom/test-utils";
+
+describe("Actionbar", () => {
+  it("renders correctly", () => {
+    const { container } = render(<Actionbar />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders with correct dropdown", () => {
+    const { getByRole } = render(<Actionbar />);
+    expect(getByRole("combobox")).toBeInTheDocument();
+    expect(getByRole("combobox")).toHaveAttribute("aria-label", "Sort by");
+    expect(screen.queryByText("Ascending")).not.toBeInTheDocument();
+    expect(screen.queryByText("Descending")).not.toBeInTheDocument();
+  });
+
+  it("renders option after clicking dropdown", async () => {
+    render(<Actionbar />);
+
+    const dropdown = screen.getByRole("combobox");
+
+    act(() => {
+      fireEvent.mouseDown(dropdown);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText("Ascending")).toBeInTheDocument();
+      expect(screen.getByText("Descending")).toBeInTheDocument();
+    });
+  });
+
+  it("renders with correct searchbar", () => {
+    const { getByRole } = render(<Actionbar />);
+    expect(getByRole("textbox")).toBeInTheDocument();
+    expect(getByRole("textbox")).toHaveAttribute("aria-label", "Searchbar");
+  });
+});
