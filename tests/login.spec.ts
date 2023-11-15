b3ae881fb099b9489c8d667c995431a257775c8b
Frederik Andreas Brunvoll Farstad <frederikfarstad@gmail.com>
2023-11-15 19:00:29 +0100

    test: implement beer voting and comment test
    ref: #34

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,8 @@
 import { expect, test } from "@playwright/test";
-import { afterEach } from "node:test";
 
 //Delete the user from the database;
 const deleteUser = async (userId: string) => {
   const query = { query: `{ deleteUser(userId: ${userId}) }` };
-
-  console.log("Deleting user with id:", userId);
 
   return await fetch("http://it2810-15.idi.ntnu.no:4000/user", {
     method: "POST",
@@ -23,7 +20,6 @@ const deleteUser = async (userId: string) => {
     })
     .then((data) => {
       expect(data.data.deleteUser).toBe("You deleted your user!");
-      console.log("User deleted:", data.data.deleteUser);
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
@@ -31,42 +27,49 @@ const deleteUser = async (userId: string) => {
 };
 
 test("login", async ({ page }) => {
+  console.log("\n");
   await page.goto("http://it2810-15.idi.ntnu.no/project2/");
 
   //Shal automatically jump to login page as it is not logged in yet
+  console.log("####### Testing redirect to login page");
   await page.getByLabel("Username").click();
   await page.getByLabel("Username").fill("E2EUser");
   await page.getByRole("button", { name: "Submit" }).click();
 
+  console.log("####### Testing correct greeting message");
   //User is already in database, so welcome back message should be displayed
   await expect(page.getByText("Created new user E2EUser!")).toBeVisible();
 
   //Redirects to the home page
+  console.log("####### Testing redirect to home page");
   await expect(
     page.getByRole("heading", { name: "Welcome, E2EUser" })
   ).toBeVisible();
 
+  console.log(
+    "####### Testing correct greeting message when already logged in"
+  );
   //When trying to go back to , so welcome back message should be displayed
   await page.goto("http://it2810-15.idi.ntnu.no/project2/login");
   await expect(page.getByText("Welcome back E2EUser!")).toBeVisible();
 
   //E2EUser is already logged in, so it should be redirected to the home page
+  console.log("####### Testing redirect to home page when already logged in");
   await expect(
     page.getByRole("heading", { name: "Welcome, E2EUser" })
   ).toBeVisible();
 
+  console.log("####### Removing the generated user from the database");
   const storageState = page.context().storageState();
-
   const userId = (await storageState).origins[0].localStorage.filter(
     (item) => item.name === "userIdBeerBuddy"
   );
-
   deleteUser(`"${userId[0].value}"`);
-
   await page.evaluate(() => window.localStorage.clear());
 
+  console.log("####### Testing redirect to login page after logged out");
+  console.log("\n");
   //User is deleted, so it should be redirected to the login page
   await page.goto("http://it2810-15.idi.ntnu.no/project2/");
-
   await expect(page.getByText("Log in")).toBeVisible();
 });
