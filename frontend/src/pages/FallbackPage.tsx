c9043d6dc824d9186f967beeeaa496de7d75b0b8
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2023-11-20 19:44:08 +0100

    feat: fallback page for not found pages
    Closes #104
    
    Closes #104
    
    See merge request it2810-h23/Team-15/prosjekt-2!71

---
+++ frontend/src/pages/FallbackPage.tsx
@@ -0,0 +1,22 @@
+import { Button } from "antd";
+import styles from "./FallbackPage.module.css";
+
+const FallbackPage = () => {
+  return (
+    <main className={styles.outerWrapper}>
+      <div className={styles.innerWrapper}>
+        <img
+          src="/project2/teku.svg"
+          alt="BeerBuddy logo"
+          className={styles.logo}
+        />
+        <h1>The page you were looking was not found!</h1>
+        <Button type="primary" href="/project2">
+          Return to dashboard
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default FallbackPage;
