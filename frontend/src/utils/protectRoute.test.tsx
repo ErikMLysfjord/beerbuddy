8576f6ad6b4837ac1946de5384ff598de2e009fd
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2023-11-21 15:17:47 +0100

    test: improve tests
    Refs: #87

--- frontend/src/utils/protectRoute.test.tsx
+++ frontend/src/utils/protectRoute.test.tsx
@@ -1,47 +1,65 @@
 import { describe, it, expect, vi } from "vitest";
 import protectRoute from "./protectRoute";
 
+/**
+ * Mocking localStorage
+ */
 global.localStorage = {
   ...global.localStorage,
   getItem: () => "test",
   removeItem: vi.fn(() => {}),
 };
+
+const mockReplace = vi.fn(() => {});
+
+global.window.location = {
+  ...global.window.location,
+  replace: mockReplace,
+};
+
 describe("protectRoute", () => {
-  it("returns false", async () => {
-    const fetchMock = vi.spyOn(global, "fetch").mockImplementation(() =>
+  beforeEach(() => {
+    mockReplace.mockClear();
+  });
+
+  it("returns false if logged in", async () => {
+    vi.spyOn(global, "fetch").mockImplementation(() =>
       Promise.resolve({
         ok: true,
         json: () => Promise.resolve({ data: { login: [{ id: "test" }] } }),
       } as Response)
     );
     const res = await protectRoute();
+    expect(mockReplace).not.toHaveBeenCalled();
     expect(res).toBe(false);
   });
 
-  it("returns true", async () => {
-    const fetchMock = vi.spyOn(global, "fetch").mockImplementation(() =>
+  it("returns true if there are no users with username", async () => {
+    vi.spyOn(global, "fetch").mockImplementation(() =>
       Promise.resolve({
         ok: true,
         json: () => Promise.resolve({ data: { login: [] } }),
       } as Response)
     );
     const res = await protectRoute();
+    expect(mockReplace).toHaveBeenCalledWith("/project2/login");
     expect(res).toBe(true);
   });
 
-  it("returns true", async () => {
-    const fetchMock = vi.spyOn(global, "fetch").mockImplementation(() =>
+  it("returns true if localstorage is different from logged in user", async () => {
+    vi.spyOn(global, "fetch").mockImplementation(() =>
       Promise.resolve({
         ok: true,
         json: () => Promise.resolve({ data: { login: [{ id: "blabla" }] } }),
       } as Response)
     );
     const res = await protectRoute();
+    expect(mockReplace).toHaveBeenCalledWith("/project2/login");
     expect(res).toBe(true);
   });
 
   it("has no values in localstorage", async () => {
-    const fetchMock = vi.spyOn(global, "fetch").mockImplementation(() =>
+    vi.spyOn(global, "fetch").mockImplementation(() =>
       Promise.resolve({
         ok: true,
         json: () => Promise.resolve({ data: { login: [{ id: "blabla" }] } }),
@@ -51,7 +69,7 @@ describe("protectRoute", () => {
       ...global.localStorage,
       getItem: () => null,
     };
-    const res = await protectRoute();
-    expect(res).toBe(undefined);
+    await protectRoute();
+    expect(mockReplace).toHaveBeenCalledWith("/project2/login");
   });
 });
