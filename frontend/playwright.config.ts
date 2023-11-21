74dbb7df0217815cb98f9de572861da3d0a438e1
Frederik Farstad <Frederf@stud.ntnu.no>
2023-11-21 16:05:40 +0100

    test: update docs for tests, make e2e-config more stable
    ref: #111

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -10,12 +10,13 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -24,18 +25,41 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "chromiumHeaded",
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          slowMo: 500,
+        },
+      },
     },
-
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          slowMo: 200,
+        },
+      },
+    },
     {
       name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      use: {
+        ...devices["Desktop Firefox"],
+        launchOptions: {
+          slowMo: 200,
+        },
+      },
     },
 
     {
       name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      use: {
+        ...devices["Desktop Safari"],
+        launchOptions: {
+          slowMo: 200,
+        },
+      },
     },
   ],
 });
