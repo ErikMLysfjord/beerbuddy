70c4efec1eff6d8a172db6d03849faf639f04c3d
Sondre Alfnes <sondre.alfnes@gmail.com>
2023-11-27 17:11:23 +0100

    feat: mysql and sqllite support

--- backend/db.ts
+++ backend/db.ts
@@ -1,10 +1,24 @@
 import { Sequelize } from "sequelize";
+import { config } from 'dotenv';
+config();
 
 // setup connection
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.db",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE === "sqlite3"
+    ? {
+        dialect: "sqlite",
+        storage: "./database.db",
+        logging: false,
+      }
+    : {
+        dialect: "mysql",
+        host: "127.0.0.1",
+        port: Number(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER || "root",
+        password: process.env.DB_PASS || "",
+        database: process.env.DB_NAME || "beers",
+        logging: false,
+      }
+);
 
 export default sequelize;
